d9d65886fc7dec67c5f6d2a6bad937aafa1138a0
Halil İbrahim Demiröz <hlldmrz007@gmail.com>
2025-03-17 12:28:30 +0300

    window created

--- src/main.c
+++ src/main.c
@@ -4,5 +4,19 @@
 #include <SDL2/SDL.h>
 
 int main(int argc, char* argv[]){
-	printf("Hello World");
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* window;
+    window = SDL_CreateWindow("ppong",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,0);
+    
+    bool quit = false;
+    SDL_Event event;
+    while(!quit){
+        while(SDL_PollEvent(&e){
+            switch(event.type){
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+            }
+        }
+    }
 }
